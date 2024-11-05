72c6f45a72881d21105216fc8a9ea231c3a26c30
DeenoDev <deyanoklaasen@outlook.com>
2024-11-05 23:35:11 +0200

    Parent div for logo and header styled.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className='fixed top-0 left-0 z-50 w-full py-10'>
-        <div>
+        <div className='container flex h-14 items-center max-lg:px-5'>
             <a>
                 <img src="/images/xora.svg" width={115} height={55} alt="logo" />
             </a>
