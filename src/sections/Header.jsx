26f6329ffeeae45db1f7421fe1acc7a2c7522cb4
DeenoDev <deyanoklaasen@outlook.com>
2024-11-09 23:49:27 +0200

    Navlink styling for mobile devices completed.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ({ title }) => (
-  <LinkScroll className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1'>
+  <LinkScroll className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
   {title}
-
   </LinkScroll>
 )
 
