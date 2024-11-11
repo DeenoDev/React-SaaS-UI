be764462cc83a7a8e34409d3491b3eba7d480c21
DeenoDev <deyanoklaasen@outlook.com>
2024-11-11 23:23:26 +0200

    navigation parent div styling completed for mobile devices.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
                 <img src="/images/xora.svg" width={115} height={55} alt="logo" />
             </a>
 
-            <div className='w-full'>
+            <div className='w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0'>
             <div className='border-2 max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
               <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
                 <ul className='flex max-lg:block max-lg:px-12'>
