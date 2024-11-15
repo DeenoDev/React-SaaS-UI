499a98e811d4466198ba8cb562ad4e02f36d4394
DeenoDev <deyanoklaasen@outlook.com>
2024-11-16 00:00:16 +0200

    Embedded element into Hero.jsx component.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,9 @@ import { Element } from "react-scroll";
 const Hero = () => {
   return (
     <section className="relative border-2 pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-amber-400 ">
+      <Element>
+        
+      </Element>
 
     </section>
   )
