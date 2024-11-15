d425499dbbcbce8b5875973f1d69f91f3b1da5ae
DeenoDev <deyanoklaasen@outlook.com>
2024-11-15 23:55:01 +0200

    Added more responsive styling to Hero section.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="relative border-2 border-amber-400 ">
+    <section className="relative border-2 pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-amber-400 ">
 
     </section>
   )
