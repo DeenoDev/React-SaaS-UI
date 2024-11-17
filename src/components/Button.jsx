f2869f710e8dbbcf16e87e15562f7f4a455f37f1
DeenoDev <deyanoklaasen@outlook.com>
2024-11-17 23:55:49 +0200

    Used clsx function to provide dynamic styling to button in Button.jsx component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import clsx from "clsx";
 
 const Button = ({ icon, children }) => {
-  return <button className="clsx()"></button>;
+  return (
+    <button className={clsx("relative, p-0.5 g5 rounded-2xl shadow-500 group")}>
+      TEXT
+    </button>
+  );
 };
 
 export default Button;
