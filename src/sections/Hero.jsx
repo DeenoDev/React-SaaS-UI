56609f5c37b920954c7bcec567135a22c66e551a
DeenoDev <deyanoklaasen@outlook.com>
2024-11-17 23:40:54 +0200

    Imported Link as Linkscroll into Hero.jsx section.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Element } from "react-scroll";
+import { Element, Link as LinkScroll } from "react-scroll";
 
 const Hero = () => {
   return (
@@ -13,14 +13,15 @@ const Hero = () => {
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
               Amazingly simple
             </h1>
-            <p className="max-w-440 mb-14 body-1 max-md:mb-10">We designed Xora AI Video Editor to be an easy to use, quick to learn, and surprisingly powerful.</p>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed Xora AI Video Editor to be an easy to use, quick to
+              learn, and surprisingly powerful.
+            </p>
           </div>
         </div>
-        
       </Element>
-
     </section>
-  )
+  );
 };
 
 export default Hero;
