19385193a023e650c95420a98a69ba1bddc1ecd8
DeenoDev <deyanoklaasen@outlook.com>
2024-11-18 23:40:45 +0200

    Added value to href attribute.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,12 +4,14 @@ import clsx from "clsx";
 const Button = ({ icon, children, href, containerClassName }) => {
   return href ? (
     <a
-      href=""
+      href={href}
       className={clsx(
         "relative, p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
-    ></a>
+    >
+      TEXT
+    </a>
   ) : (
     <button
       className={clsx(
