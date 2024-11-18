ab553c8d121a0f8345d4724a3ef266153739686f
DeenoDev <deyanoklaasen@outlook.com>
2024-11-18 23:43:09 +0200

    Added onClick eventListener and Prop to Button.jsx component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 
-const Button = ({ icon, children, href, containerClassName }) => {
+const Button = ({ icon, children, href, containerClassName, onClick }) => {
   return href ? (
     <a
       href={href}
@@ -18,6 +18,7 @@ const Button = ({ icon, children, href, containerClassName }) => {
         "relative, p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
+      onClick={onClick}
     >
       TEXT
     </button>
