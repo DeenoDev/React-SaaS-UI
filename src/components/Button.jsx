1b7e16743c1de87da95038466536e86fcdc5561f
DeenoDev <deyanoklaasen@outlook.com>
2024-11-21 23:54:31 +0200

    positioned marker with position absolute.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,7 +13,7 @@ const Button = ({
   const Inner = () => (
     <>
       <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
-        <span>
+        <span className="absolute -left-[1px]">
           <Marker markerFill={markerFill} />
         </span>
         {icon && (
