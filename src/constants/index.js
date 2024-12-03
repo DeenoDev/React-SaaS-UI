2aa9a7a3ddbf5d9145fd52649fad6aba506454cf
DeenoDev <deyanoklaasen@outlook.com>
2024-12-03 23:39:17 +0200

    Created features constant inside index.js file with an array of 2 objects.

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+const features = [
+    {
+        id: '0',
+        icon: '/images/feature-1.png',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 1',
+        text: 'TEXT 1',
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the demo'
+        }
+    },
+    {
+        id: '0',
+        icon: '/images/feature-1.png',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 1',
+        text: 'TEXT 1',
+        button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the demo'
+        }
+    }
+]
