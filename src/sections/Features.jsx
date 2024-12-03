5f221ce3a56631b40661d101af130ab5040f9b53
DeenoDev <deyanoklaasen@outlook.com>
2024-12-03 23:33:05 +0200

    Created an array of objects that will contain all content for Features section.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -4,7 +4,26 @@ import { Element } from "react-scroll";
 const Features = () => {
   return (
     <section>
-      <Element name="features" />
+      <Element name="features">
+        <div className="container">
+          <div className="relative flex">
+            {[
+              {
+                icon: "test",
+                caption: "caption",
+                caption: "caption",
+                caption: "caption",
+              },
+              {
+                icon: "test",
+                caption: "caption",
+                caption: "caption",
+                caption: "caption",
+              },
+            ]}
+          </div>
+        </div>
+      </Element>
     </section>
   );
 };
