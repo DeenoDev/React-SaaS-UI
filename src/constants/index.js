0c68ed1a4735a59bf1c1cb936ec2ee4aab8fed9f
DeenoDev <deyanoklaasen@outlook.com>
2024-12-03 23:40:19 +0200

    Completed all specified numerals for features objects.

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,11 +11,11 @@ const features = [
         }
     },
     {
-        id: '0',
-        icon: '/images/feature-1.png',
+        id: '1',
+        icon: '/images/feature-2.png',
         caption: 'DEMO CAPTION',
-        title: 'FEATURE 1',
-        text: 'TEXT 1',
+        title: 'FEATURE 2',
+        text: 'TEXT 2',
         button: {
             icon: '/images/magictouch.svg',
             title: 'Watch the demo'
