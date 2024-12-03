142dea3f577784ae3e19eddef65992e32c101a4d
DeenoDev <deyanoklaasen@outlook.com>
2024-12-03 23:55:49 +0200

    Completed styling for features parent div.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -7,7 +7,7 @@ const Features = () => {
     <section>
       <Element name="features">
         <div className="container">
-          <div className="relative flex">
+          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map((feature) => (
               <div key={feature.id}>{feature.title}</div>
             ))}
