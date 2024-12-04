c560aea093832a59225a0d37e95e845d2555763e
DeenoDev <deyanoklaasen@outlook.com>
2024-12-04 23:56:32 +0200

    Added styling to rendered feature divs.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -9,7 +9,9 @@ const Features = () => {
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map((feature) => (
-              <div key={feature.id}>{feature.title}</div>
+              <div key={feature.id} className="relative z-2 md:px-2 px-5">
+                {feature.title}
+              </div>
             ))}
           </div>
         </div>
