1074231fdb9c4ad5859315d0660d54fb2b3e9570
DeenoDev <deyanoklaasen@outlook.com>
2024-12-13 23:32:06 +0200

    Rendered destructured feature properties.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -10,12 +10,12 @@ const Features = () => {
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map(({ id, icon, caption, title, text, button }) => (
               <div
-                key={feature.id}
+                key={id}
                 className="relative z-2 md:px-2 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-3 max-md:rounded-3xl max-md:flex-320"
               >
                 <div className="w-full flex justify-start items-start">
                   <div className="">
-                    <img src={feature.icon} alt="" />
+                    <img src={icon} alt="" />
                   </div>
                 </div>
                 {/* {feature.title} */}
