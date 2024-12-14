3d1febe169b9cff25527cde7738d612b51cf2873
DeenoDev <deyanoklaasen@outlook.com>
2024-12-14 23:50:40 +0200

    Styled child div for undordered list.

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -33,7 +33,9 @@ const Features = () => {
               </div>
             ))}
 
-            <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden"></ul>
+            <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden">
+              <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10" />
+            </ul>
           </div>
         </div>
       </Element>
