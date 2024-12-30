b1348be1c422ea8701368d56ae2139e1fd3a9f6c
DeenoDev <deyanoklaasen@outlook.com>
2024-12-30 23:57:19 +0200

    Added more props to LinkScroll embedded component.

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -4,7 +4,14 @@ import { useState } from "react";
 import clsx from "clsx";
 
 const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+  <LinkScroll
+    to={title}
+    offset={-100}
+    spy
+    smooth
+    activeClass="nav-active"
+    className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+  >
     {title}
   </LinkScroll>
 );
