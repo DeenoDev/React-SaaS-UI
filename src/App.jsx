e5334a259c4c6746497e041a45cadebf73d3d1fe
DeenoDev <deyanoklaasen@outlook.com>
2025-01-02 23:54:45 +0200

    Pricing.jsx section imported into App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
 
 const App = () => {
   return (
