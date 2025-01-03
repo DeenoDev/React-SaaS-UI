81e2f4c628b082846103a1a60957c70643e399b0
DeenoDev <deyanoklaasen@outlook.com>
2025-01-03 23:50:09 +0200

    Added h3 heading with responsive size.

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -5,7 +5,13 @@ const Pricing = () => {
   return (
     <section>
       <Element name="pricing">
-        <div className="container"></div>
+        <div className="container">
+          <div className="max-w-[950px] border-2 border-red-500">
+            <h3 className="h3 max-lg:h4 max-md:h5 ">
+              Flexible pricing for teams of all sizes.
+            </h3>
+          </div>
+        </div>
       </Element>
     </section>
   );
