468c9f4c6c03bff567f0d3fb0ef7d97a0d4c8426
DeenoDev <deyanoklaasen@outlook.com>
2025-04-02 22:56:53 +0200

    Added responsive styling to pricing parent div.

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -14,7 +14,7 @@ const Pricing = () => {
               Flexible pricing for teams of all sizes.
             </h3>
 
-            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px]">
+            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
               <button
                 className={clsx("pricing-head_btn", monthly && "text-p4")}
                 onClick={() => setMonthly(true)}
