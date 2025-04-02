aecdc2e0be361281b8ec86abe81279daa0b271e7
DeenoDev <deyanoklaasen@outlook.com>
2025-04-02 22:38:54 +0200

    styled parent div of prices section.

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -13,7 +13,8 @@ const Pricing = () => {
             <h3 className="h3 max-lg:h4 max-md:h5 z-3 mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
               Flexible pricing for teams of all sizes.
             </h3>
-            <div>
+
+            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px]">
               <button
                 className={clsx("pricing-head_btn", monthly && "text-p4")}
                 onClick={() => setMonthly(true)}
