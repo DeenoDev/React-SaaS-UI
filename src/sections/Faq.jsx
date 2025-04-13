85e27eb2838720d8daf2b82bd14621ce09ef8fac
DeenoDev <deyanoklaasen@outlook.com>
2025-04-13 23:37:51 +0200

    Styled paragraph tag in Faq section.

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -9,7 +9,9 @@ const Faq = () => {
           <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
             Curiosity didn't kill the cat, it gave it answers.
           </h3>
-          <p>You've got quetions, we've got answers.</p>
+          <p className="body-1 max-lg:max-w-sm">
+            You've got quetions, we've got answers.
+          </p>
         </div>
       </Element>
     </section>
