bdb9a70fb9dcaf33d22f6407c27a853a8289d6a0
DeenoDev <deyanoklaasen@outlook.com>
2025-04-13 10:47:58 +0200

    Imported Faq section into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 const App = () => {
   return (
