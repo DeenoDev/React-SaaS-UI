9b9cef07e3d1a45297553645561539ecb47b762b
DeenoDev <deyanoklaasen@outlook.com>
2025-04-13 10:43:11 +0200

    Fixed pricing icon postions on mobile devices anes and completed pricing section.

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -12,9 +12,9 @@ const Pricing = () => {
     <section>
       <Element name="pricing">
         <div className="container">
-          <div className="max-w-[950px] pricing-head_before relative mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16 ">
-            <h3 className="h3 max-lg:h4 max-md:h5 z-3 mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
-              Flexible pricing for teams of all sizes.
+          <div className="max-w-960 pricing-head_before relative mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16">
+            <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
+              Flexible pricing for teams of all sizes
             </h3>
 
             <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
@@ -30,6 +30,7 @@ const Pricing = () => {
               >
                 Annual
               </button>
+
               <div
                 className={clsx(
                   "g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500",
@@ -55,7 +56,8 @@ const Pricing = () => {
               />
             </div>
           </div>
-          {/* Pricing Section */}
+
+          {/*  pricing section*/}
           <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6">
             {plans.map((plan, index) => (
               <div
@@ -65,10 +67,11 @@ const Pricing = () => {
                 {index === 1 && (
                   <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
                 )}
+
                 <div
                   className={clsx(
                     "absolute left-0 right-0 z-2 flex items-center justify-center",
-                    index === 1 ? "-top-6" : "-top-6: xl:-top-11"
+                    index === 1 ? "-top-6" : "-top-6 xl:-top-11"
                   )}
                 >
                   <img
