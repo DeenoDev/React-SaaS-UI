b960d0fcba03fdfce54bd0259bb7347bb7b5d445
DeenoDev <deyanoklaasen@outlook.com>
2025-04-13 23:30:16 +0200

    Added h3 heading and paragraph to Faq section.

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Faq = () => {
   return (
     <section>
-      <Element name="faq" className="relative"></Element>
+      <Element name="faq" className="relative">
+        <div>
+          <h3>Curiosity didn't kill the cat, it gave it answers.</h3>
+          <p>You've got quetions, we've got answers.</p>
+        </div>
+      </Element>
     </section>
   );
 };
