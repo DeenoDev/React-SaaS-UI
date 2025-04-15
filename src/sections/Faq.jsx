d37e9b2cb252467db4c72ea8a0fe5a4077fadfa2
DeenoDev <deyanoklaasen@outlook.com>
2025-04-15 22:46:26 +0200

    Imported faq from constants.

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Element } from "react-scroll";
+import { faq } from "../constants/index.js";
 
 const Faq = () => {
   return (
@@ -23,6 +24,8 @@ const Faq = () => {
             <div className="rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
               <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
             </div>
+
+            <div className="relative flex-1 pt-24">{faq}</div>
           </div>
         </div>
       </Element>
