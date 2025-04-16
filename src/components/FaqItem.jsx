24ac335cc7f2b471eb087edd78ef7fea520bdce3
DeenoDev <deyanoklaasen@outlook.com>
2025-04-16 22:20:28 +0200

    Rendered 0 or nothing using conditional logic.

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const FaqItem = ({ item, index }) => {
-  return <div className="relative z-2 mb-16">FaqItem</div>;
+  return (
+    <div className="relative z-2 mb-16">
+      <div
+        className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
+        // onClick{() => {}}
+      >
+        <div className="flex-1">
+          <div>{index < 10 ? "0" : ""}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FaqItem;
