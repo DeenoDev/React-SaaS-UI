8f694a67ceaab1268b94167b3565db01abd1516b
DeenoDev <deyanoklaasen@outlook.com>
2025-04-24 23:37:46 +0200

    Added text heading for Testimonials section.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -4,7 +4,10 @@ const Testimonials = () => {
   return (
     <section className="relative z-2 py-24 md:py-28 lg:py-40">
       <div className="container block lg:flex">
-        <div className="testimonials_head-res relative z-2 mr-20 flex-300"></div>
+        <div className="testimonials_head-res relative z-2 mr-20 flex-300">
+          <p>Wall of Love</p>
+          <h3>Words from our fans</h3>
+        </div>
       </div>
     </section>
   );
