8801410022b7207cae43a0e68a40cf93bbfb306b
DeenoDev <deyanoklaasen@outlook.com>
2025-04-24 23:27:26 +0200

    Added basic styling to Testimonials parent section.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return <section className="relative z-2 py-24 md:py-28 lg:py-40"></section>;
 };
 
 export default Testimonials;
