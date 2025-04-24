bd47613af521b3c7deb8183cf807c4dc0ec8eac7
DeenoDev <deyanoklaasen@outlook.com>
2025-04-24 23:36:09 +0200

    Added styling to child divs of Testimonials parent section.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <section className="relative z-2 py-24 md:py-28 lg:py-40"></section>;
+  return (
+    <section className="relative z-2 py-24 md:py-28 lg:py-40">
+      <div className="container block lg:flex">
+        <div className="testimonials_head-res relative z-2 mr-20 flex-300"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
