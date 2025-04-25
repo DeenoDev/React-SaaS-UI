773fb5adf3949bc2043a202ffd33165015cf3cc6
DeenoDev <deyanoklaasen@outlook.com>
2025-04-25 23:51:56 +0200

    Rendered testimonials.

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
         </div>
 
         <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
-          <div className="testimonials_group-after flex-50"></div>
+          <div className="testimonials_group-after flex-50">{testimonials}</div>
         </div>
       </div>
     </section>
