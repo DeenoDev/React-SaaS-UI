bc18491b3413cf33aa469799f07ac252f54ab52a
DeenoDev <deyanoklaasen@outlook.com>
2025-05-03 22:01:50 +0200

    Added styling to TestimonialItem.jsx component.

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import clsx from "clsx";
 
 const TestimonialItem = ({ item, containerClassName }) => {
-  return <div className={clsx("relative px-14 ")}>TestimonialItem</div>;
+  return (
+    <div className={clsx("relative px-14 pt-11 pb-14")}>TestimonialItem</div>
+  );
 };
 
 export default TestimonialItem;
