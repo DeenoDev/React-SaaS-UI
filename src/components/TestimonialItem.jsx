42cd67003b7efe6d6584663571ad5a62bc9d2380
DeenoDev <deyanoklaasen@outlook.com>
2025-05-03 22:04:16 +0200

    Used psuedo elements to create lines in between testimonials.

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -3,7 +3,13 @@ import clsx from "clsx";
 
 const TestimonialItem = ({ item, containerClassName }) => {
   return (
-    <div className={clsx("relative px-14 pt-11 pb-14")}>TestimonialItem</div>
+    <div
+      className={clsx(
+        "relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 "
+      )}
+    >
+      TestimonialItem
+    </div>
   );
 };
 
