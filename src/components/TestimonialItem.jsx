88fa3d569f1f65fcddad4c43a987e6d1720766fb
DeenoDev <deyanoklaasen@outlook.com>
2025-05-06 23:16:55 +0200

    Styled parent div of avatar images.

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -12,7 +12,7 @@ const TestimonialItem = ({ item, containerClassName }) => {
       <blockquote className="h6 mb-8 text-p4">{item.comment}</blockquote>
 
       <div className="flex items-center max-xl:-mr-8">
-        <div>
+        <div className="mr-4 size-20 shrink-0 rounded-half border-2 border-s2 p-1.5">
           <img
             src={item.avatarUrl}
             alt={item.name}
