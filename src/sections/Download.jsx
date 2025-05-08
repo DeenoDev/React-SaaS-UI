35d3113cb2b9e85a4e0102640b390090a9e0a295
DeenoDev <deyanoklaasen@outlook.com>
2025-05-08 23:38:27 +0200

    Styled section to have background gradient in Download.jsx section.

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -3,7 +3,7 @@ import { Element } from "react-scroll";
 
 const Download = () => {
   return (
-    <section>
+    <section name="download" className="g7 relative pb-32 pt-24">
       <Element></Element>
     </section>
   );
