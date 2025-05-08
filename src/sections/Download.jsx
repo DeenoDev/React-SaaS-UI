544ad2af8a7b157cd910516ed7d356f7c34dc34d
DeenoDev <deyanoklaasen@outlook.com>
2025-05-08 23:47:26 +0200

    Added more styling to child elements of scrollable Element tag.

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -7,7 +7,13 @@ const Download = () => {
       <Element
         name="download"
         className="g7 relative pb-32 pt-24 max-lg:pb-24 max-md:py-16"
-      ></Element>
+      >
+        <div className="container">
+          <div className="flex items-center">
+            <div className="relative mr-6 flex-540 max-xl:flex-280 max-lg:flex-256 max-md:flex-100"></div>
+          </div>
+        </div>
+      </Element>
     </section>
   );
 };
