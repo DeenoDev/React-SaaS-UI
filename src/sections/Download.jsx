7c2df50e5fd53beb03932572b98cea537283d39a
DeenoDev <deyanoklaasen@outlook.com>
2025-05-08 23:39:52 +0200

    Added responsive styling to Download.jsx parent section.

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -3,7 +3,10 @@ import { Element } from "react-scroll";
 
 const Download = () => {
   return (
-    <section name="download" className="g7 relative pb-32 pt-24">
+    <section
+      name="download"
+      className="g7 relative pb-32 pt-24 max-lg:pb-24 max-md:py-16"
+    >
       <Element></Element>
     </section>
   );
