d9bfa28aa3344de85f1dcb33b733979451c269cb
DeenoDev <deyanoklaasen@outlook.com>
2025-05-27 23:16:05 +0200

    Added styling to list item in Downloads.jsx section.

--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -72,7 +72,7 @@ const Download = () => {
           </div>
           <ul className="mt-24 flex justify-center max-lg:hidden">
             {logos.map(({ id, url, width, height, title }) => (
-              <li>
+              <li key={id} className="mx-10">
                 <img src={url} width={width} height={height} alt={title} />
               </li>
             ))}
