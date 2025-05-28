c410d263ff9bf4abccb3a3361d72e3997f45e224
DeenoDev <deyanoklaasen@outlook.com>
2025-05-28 23:22:06 +0200

    Rendered semantic footer tag along with container div.

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container py-10"></div>
+    </footer>
+  );
 };
 
 export default Footer;
