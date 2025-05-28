861827adbb1bd3964a0fd1c7a046d5cdcdb28c6a
DeenoDev <deyanoklaasen@outlook.com>
2025-05-28 23:32:18 +0200

    Added copyright symbol, Privacy policy and terms of use to Footer.jsx section.

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
       <div className="container py-10">
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="opacity-70">Copyright, Deyano Klaasen</p>
+            <p className="opacity-70">Copyright &copy; Deyano Klaasen</p>
+            <div className="flex items-center justify-center sm:ml-auto">
+              <p>Privacy Policy</p>
+              <p>Terms of Use</p>
+            </div>
           </div>
         </div>
       </div>
